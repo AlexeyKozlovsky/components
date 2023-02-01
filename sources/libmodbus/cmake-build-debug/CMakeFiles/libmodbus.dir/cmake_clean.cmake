3e01d8c372fec7f46987737a9ae2e980d223c568
AlexeyKozlovsky <alan-none@yandex.ru>
2023-02-01 13:22:29 +0300

    fixed

---
+++ sources/libmodbus/cmake-build-debug/CMakeFiles/libmodbus.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libmodbus.dir/main.cpp.o"
+  "CMakeFiles/libmodbus.dir/main.cpp.o.d"
+  "CMakeFiles/libmodbus.dir/modbus.cpp.o"
+  "CMakeFiles/libmodbus.dir/modbus.cpp.o.d"
+  "liblibmodbus.pdb"
+  "liblibmodbus.so"
+  "liblibmodbus.so.1"
+  "liblibmodbus.so.1.0.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libmodbus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

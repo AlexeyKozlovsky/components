c91a1575e4480de90f01791c369963550f75d3a5
AlexeyKozlovsky <alan-none@yandex.ru>
2024-03-12 17:17:12 +0300

    Подготовил к сборке под linux в библиотеке threadpooling

---
+++ sources/libthreadpooling/env-linux.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_INSTALL_PREFIX "/home/tango/Documents/libs")
+message("FFFF")
+
+set(CMAKE_DEBUG_POSTFIX d)
+
+set(BASE_INSTALLED_DIR "/home/tango/Documents/libs")
+
+set(utils_DIR "${BASE_INSTALLED_DIR}/lib/cmake/utils")
